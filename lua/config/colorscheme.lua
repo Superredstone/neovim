02982a8f7f91f4f1b6b5fd320596329ca7205305
Superredstone <patrickcanal3@gmail.com>
2024-08-11 07:13:48 +0200

    Updated themery config

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,11 +1,3 @@
-local theme_config_file = "";
-
-if vim.fn.has("win32") == 1 then
-	theme_config_file = vim.env.HOME .. "\\AppData\\Local\\nvim\\lua\\config\\colorscheme.lua"
-else
-	theme_config_file = "~/.config/nvim/lua/config/colorscheme.lua"
-end
-
 require("themery").setup({
 	themes = {
 		{
@@ -30,14 +22,5 @@ require("themery").setup({
 		}
 	},
 	livePreview = true,
-	themeConfigFile = theme_config_file
 })
 
--- Default colorscheme
-vim.cmd([[colorscheme onedark]])
-
--- Themery block
--- This block will be replaced by Themery.
-vim.cmd("colorscheme catppuccin-mocha")
-vim.g.theme_id = 4
--- end themery block
