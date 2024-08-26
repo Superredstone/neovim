df89b03e53883f5657ed34b3ee30b92efcc29acc
Superredstone <patrickcanal3@gmail.com>
2024-08-26 08:09:29 +0200

    Added macro keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,8 +4,8 @@ local opts = { noremap = true, silent = true }
 map('n', '<A-l>', ':bnext<CR>', opts) -- Next buffer
 map('n', '<A-h>', ':bprevious<CR>', opts) -- Last buffer
 map('n', '<C-s>', ':w<CR>', opts) -- Save
-map('n', '<C-q>', ':q<CR>', opts) -- Quit and save
-map('n', 'q', ':bdelete<CR>', opts) -- Close current buffer
+map('n', '<C-q>', ':q<CR>', opts) -- Quit
+map('n', '<S-q>', ':bdelete<CR>', opts) -- Close current buffer
 map('n', '<Space>e', ':NvimTreeToggle<CR>', opts) -- Open file tree
 map('n', '<C-c>', ':CommentToggle<CR>', opts) -- Toggle comment n mode
 map('v', '<C-c>', ':CommentToggle<CR>', opts) -- Toggle comment v mode
