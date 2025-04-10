c29c0cf278ad499800c510f7f48e40e8a3be25ed
Superredstone <patrickcanal3@gmail.com>
2025-04-10 14:12:57 +0200

    Added some lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,3 +6,9 @@ lsp.gopls.setup({}) -- Go
 lsp.jdtls.setup({}) -- Java
 lsp.lua_ls.setup({cmd = { "lua-lsp" }}) -- Lua
 lsp.rust_analyzer.setup({}) -- Rust
+lsp.ocamllsp.setup({}) -- Ocaml
+lsp.zls.setup({}) -- Zig
+lsp.nim_langserver.setup({}) -- Nim
+lsp.pylsp.setup({}) -- Python
+lsp.emmet_language_server.setup({}) -- HTML
+lsp.ts_ls.setup({}) -- JS, TS
